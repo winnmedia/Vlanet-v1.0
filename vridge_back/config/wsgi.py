98968dfd7084e2a8ec9524a98c50c5ef7248eced
VideoPlanet <dev@videoplanet.com>
2025-07-04 01:18:20 +0900

    fix: Railway 배포 설정 간소화
    - wsgi.py에서 항상 settings_minimal 사용하도록 수정
    - railway.json의 startCommand 간소화
    - 환경변수 의존성 제거

--- vridge_back/config/wsgi.py
+++ vridge_back/config/wsgi.py
@@ -9,13 +9,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 
-# 환경 변수에서 설정 모듈을 받아오거나 기본값 사용
-if os.environ.get('RAILWAY_ENVIRONMENT'):
-    # Railway 환경에서는 환경변수 또는 minimal 설정 사용
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", os.environ.get('DJANGO_SETTINGS_MODULE', "config.settings_minimal"))
-else:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings_base")
-
 from django.core.wsgi import get_wsgi_application
+
+# Railway 환경에서는 항상 settings_minimal 사용
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings_minimal")
 
 application = get_wsgi_application()

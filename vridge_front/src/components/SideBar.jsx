66baaf9ae40d26a1070b8b292ab939876a122878
VideoPlanet <dev@videoplanet.com>
2025-07-15 01:18:55 +0900

    remove: 프레임워크 관리 메뉴 제거
    - SideBar에서 프레임워크 관리 메뉴 삭제
    - 불필요한 기능 제거

--- vridge_front/src/components/SideBar.jsx
+++ vridge_front/src/components/SideBar.jsx
@@ -138,15 +138,6 @@ const SideBar = memo(function SideBar({ tab, on_menu }) {
               전체 일정
             </li>
             <li
-              className={cx({ active: path === '/FrameworkManagement' && !SubMenu })}
-              onClick={() => {
-                SetSubMenu(false)
-                navigate('/FrameworkManagement')
-              }}
-            >
-              프레임워크 관리
-            </li>
-            <li
               className={cx('menu_project', 'has-toggle', {
                 active:
                   path.includes('/ProjectView') ||

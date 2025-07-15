752898688fe1872d3f551adf4a0b643ca70815e7
VideoPlanet <dev@videoplanet.com>
2025-07-15 12:32:50 +0900

    fix: 피드백 페이지 버튼 크기 통일 - 모든 버튼에 동일한 padding과 높이 적용

--- vridge_front/src/page/Cms/Feedback.jsx
+++ vridge_front/src/page/Cms/Feedback.jsx
@@ -1468,9 +1468,8 @@ export default function Feedback() {
                         onClick={DeleteFile}
                         className={styles.feedbackButtonDanger}
                         title="영상 삭제"
-                        style={{ padding: '10px' }}
                       >
-                        <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                           <path d="M3 6h18M8 6V4a2 2 0 012-2h4a2 2 0 012 2v2m3 0v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6h14zM10 11v6M14 11v6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                         </svg>
                         <span>삭제</span>
@@ -1483,9 +1482,8 @@ export default function Feedback() {
                         onClick={() => CopyFileUrl(current_project.files)}
                         className={styles.feedbackButtonSecondary}
                         title="공유"
-                        style={{ padding: '10px' }}
                       >
-                        <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                           <path d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                         </svg>
                         <span>공유</span>
